98747669a40e4ed58d5256a00294e551c4d2b781
HuangZhe <3451701311@qq.com>
2023-09-28 11:15:06 +0800

    feat: 增加 `mermaid`主题, 增加防抖节流侧边栏

--- packages/notes/src/.vitepress/config.ts
+++ packages/notes/src/.vitepress/config.ts
@@ -3,45 +3,49 @@ import { imgLazyload } from "@mdit/plugin-img-lazyload";
 import { tasklist } from "@mdit/plugin-tasklist";
 import { chineseSearchOptimize, pagefindPlugin } from "vitepress-plugin-pagefind";
 import { attrs } from "@mdit/plugin-attrs";
+import { withMermaid } from "vitepress-plugin-mermaid";
 
 const baseUrl: string = "/record";
 // https://vitepress.dev/reference/site-config
-export default defineConfig({
-  base: baseUrl,
-  title: "我的笔记",
-  description: "记录自己笔记的地方",
-  head: [["link", { rel: "icon", href: baseUrl + "/learning.svg" }]],
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: "然叔", link: "/ranshu/loader" },
-      { text: "杂谈", link: "/others/HumanSystemOptimization" },
-      { text: "CSS", link: "/css/bfc" },
-      { text: "JavaScript", link: "/javascript/eventLoop" },
-    ],
+export default withMermaid({
+	base: baseUrl,
+	title: "我的笔记",
+	description: "记录自己笔记的地方",
+	head: [["link", { rel: "icon", href: baseUrl + "/learning.svg" }]],
+	themeConfig: {
+		// https://vitepress.dev/reference/default-theme-config
+		nav: [
+			{ text: "然叔", link: "/ranshu/loader" },
+			{ text: "杂谈", link: "/others/HumanSystemOptimization" },
+			{ text: "CSS", link: "/css/bfc" },
+			{ text: "JavaScript", link: "/javascript/eventLoop" },
+		],
 
-    sidebar: {
-      "/ranshu": [
-        { text: "webpack常见的loader", link: "/ranshu/loader" },
-        { text: "webpack常见的plugin", link: "/ranshu/plugin" },
-        { text: "提升前端构建工具的性能", link: "/ranshu/performanceImprovements" },
-      ],
-      "/others": [{ text: "身体系统调优不完全指南", link: "/others/HumanSystemOptimization" }],
-      "/css": [{ text: "CSS 中的 BFC", link: "/css/bfc" }],
-      "/javascript": [{ text: "并发模型和事件循环", link: "/javascript/eventLoop" }],
-    },
+		sidebar: {
+			"/ranshu": [
+				{ text: "webpack常见的loader", link: "/ranshu/loader" },
+				{ text: "webpack常见的plugin", link: "/ranshu/plugin" },
+				{ text: "提升前端构建工具的性能", link: "/ranshu/performanceImprovements" },
+			],
+			"/others": [{ text: "身体系统调优不完全指南", link: "/others/HumanSystemOptimization" }],
+			"/css": [{ text: "CSS 中的 BFC", link: "/css/bfc" }],
+			"/javascript": [
+				{ text: "并发模型和事件循环", link: "/javascript/eventLoop" },
+				{ text: "防抖和节流", link: "/javascript/debounceAndThrottle" },
+			],
+		},
 
-    socialLinks: [{ icon: "github", link: "https://github.com/Zhe28" }],
-  },
-  markdown: {
-    toc: { level: [1, 2, 3, 4] },
-    config: (md) => {
-      md.use(imgLazyload).use(tasklist, {}).use(attrs, {});
-    },
-  },
-  // outDir: "../dist",
-  vite: {
-    build: { target: "esnext", emptyOutDir: true, assetsInlineLimit: 409600 },
-    plugins: [pagefindPlugin({ customSearchQuery: chineseSearchOptimize })],
-  },
+		socialLinks: [{ icon: "github", link: "https://github.com/Zhe28" }],
+	},
+	markdown: {
+		toc: { level: [1, 2, 3, 4] },
+		config: (md) => {
+			md.use(imgLazyload).use(tasklist).use(attrs);
+		},
+	},
+	// outDir: "../dist",
+	vite: {
+		build: { target: "esnext", emptyOutDir: true, assetsInlineLimit: 409600 },
+		plugins: [pagefindPlugin({ customSearchQuery: chineseSearchOptimize })],
+	},
 });
