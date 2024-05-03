b42849c78225f418445067f562d2c7a2a01922cd
Huang Zhe <3451701311@qq.com>
2024-05-03 11:13:51 +0800

    configure: 添加首页重定向效果
    把 /record/ 重定向到 /record/notes/src/

--- packages/.vitepress/theme/index.ts
+++ packages/.vitepress/theme/index.ts
@@ -1,6 +1,14 @@
 import Theme from "vitepress/theme";
 import "./index.less";
+import { watch } from "vue";
 
 export default {
   ...Theme,
+  enhanceApp({ app, router, siteData }) {
+    watch(router.route, () => {
+      if (router.route.path === "/record/") {
+        router.go("/record/notes/src/");
+      }
+    });
+  },
 };
