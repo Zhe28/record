083e866132d466b755959e9e74eb7601b4c67627
zhe28 <huangze28@foxmail.com>
2024-09-30 14:37:56 +0800

    chore[package]：添加图片缩放的功能
    1. 使用@fancyapps/ui添加这个功能

--- packages/.vitepress/theme/index.ts
+++ packages/.vitepress/theme/index.ts
@@ -1,10 +1,15 @@
 import Theme from "vitepress/theme";
 import "./index.less";
 import { watch } from "vue";
+import "@fancyapps/ui/dist/fancybox/fancybox.css"
+import { Fancybox } from "@fancyapps/ui";
 
 export default {
   ...Theme,
   enhanceApp({ app, router, siteData }) {
+    // 添加图片缩放功能
+    Fancybox.bind("img")
+
     watch(router.route, () => {
       if (router.route.path === "/record/") {
         router.go("/record/notes/src/");
